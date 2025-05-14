786b59236196127a6d919f636805eced802a2c5c
Suyan Man Amatya <srkamatya007@gmail.com>
2025-05-14 16:04:04 +0545

    order place success

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,10 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  useLocation,
+} from "react-router-dom";
 
 import { Navbar } from "./components/Navbar.jsx";
 import { Hero } from "./components/Hero.jsx";
@@ -19,41 +24,41 @@ import Inaya from "./components/iNaya/Inaya.jsx";
 import Parent from "./components/Parent/Parent.jsx";
 import StoreDetail from "./components/FoodDisplay/StoreDetail.jsx";
 import { Cart } from "./components/Cart/Cart.jsx";
+import OrderSuccess from "./components/Cart/OrderSuccess.jsx";
+
+function AppContent() {
+  const location = useLocation();
+  const hideNavbarFooter = location.pathname === "/order-success";
+
+  return (
+    <>
+      <ScrollToTop />
+      {!hideNavbarFooter && <Navbar />}
+      <Routes>
+        <Route path="/" element={<Hero />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/store" element={<FoodDisplay />} />
+        <Route path="/store/:id" element={<StoreDetail />} />
+        {/* <Route path="/JHP" element={<JHP />} />
+        <Route path="/JHCB" element={<JHCB />} />
+        <Route path="/inaya" element={<Inaya />} /> */}
+        <Route path="/parent" element={<Parent />} />
+        <Route path="/payment" element={<PlaceOrder />} />
+        <Route path="/order-success" element={<OrderSuccess />} />
+        <Route path="/payment-success" element={<Success />} />
+        <Route path="/payment-failure" element={<Failure />} />
+        <Route path="/payment-form" element={<Payment />} />
+      </Routes>
+      {!hideNavbarFooter && <Footer />}
+    </>
+  );
+}
 
 function App() {
-  const addToCart = (product) => {
-    setCart([...cart, product]);
-  };
-
   return (
     <StoreContextProvider>
       <Router>
-        {/* <PopupOffer /> */}
-        <ScrollToTop />
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Hero />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route
-            path="/store"
-            element={<FoodDisplay addToCart={addToCart} />}
-          />
-          <Route path="/store/:id" element={<StoreDetail />} />
-
-          {/* <Route path="/JHP" element={<JHP />} />
-          <Route path="/JHCB" element={<JHCB />} />
-          <Route path="/inaya" element={<Inaya />} /> */}
-
-          <Route path="/parent" element={<Parent />} />
-
-          <Route path="/payment" element={<PlaceOrder />} />
-          <Route path="/payment-success" element={<Success />} />
-          <Route path="/payment-failure" element={<Failure />} />
-          <Route path="/payment-form" element={<Payment />} />
-        </Routes>
-
-        {/* Footer */}
-        <Footer />
+        <AppContent />
       </Router>
     </StoreContextProvider>
   );
